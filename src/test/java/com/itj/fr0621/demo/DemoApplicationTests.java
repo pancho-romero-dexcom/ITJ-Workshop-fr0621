ff10c584fdce1e861eede7e2698a0edafceb5cbb
FR0621 <francisco.romero@dexcom.com>
2021-11-19 12:29:38 -0600

    first commitc

---
+++ src/test/java/com/itj/fr0621/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itj.fr0621.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
